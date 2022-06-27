44294bdffeddb80473bed2d8c39fd07302a6b431
jorenbroekema <joren.broekema@gmail.com>
2022-06-27 11:02:15 +0200

    update site

---
+++ index.js
@@ -0,0 +1,47 @@
+function mijnfunctie() {
+  var dediv = document.getElementById("onzeuitkomst");
+  //alert("het is gelukt");
+  // XMLHttpRequest   ouder
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function () {
+    if (this.readyState == 4) {
+      var antwoord = this.responseText;
+      var hetantwoord = JSON.parse(this.responseText);
+      dediv.innerHTML = hetantwoord.data[0].first_name; // objecten  NIETSTRING
+    }
+  };
+  console.log("opening");
+  xhr.open("GET", "https://reqres.in/api/users?page=2", true);
+  xhr.send();
+}
+
+mijnfunctie();
+
+async function fetchMeals() {
+  const response = await fetch("./maaltijden.json");
+  const result = await response.json();
+  return result;
+}
+
+async function displayMeals() {
+  const meals = await fetchMeals();
+  const container = document.getElementById("maaltijden");
+
+  let htmlString = "";
+  meals.forEach((meal) => {
+    htmlString += `
+      <div class="meal">
+        <img alt="${meal.description}" src="${meal.img}" />
+        <h3>${meal.name}</h3>
+        <span>${new Intl.NumberFormat(navigator.language, {
+          style: "currency",
+          currency: "EUR",
+        }).format(meal.price)}</span>
+      </div>
+    `;
+  });
+
+  container.innerHTML = htmlString;
+}
+
+displayMeals();
